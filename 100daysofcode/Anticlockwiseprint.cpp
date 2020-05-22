3c28058a348c1b30f7792fae02b7b4d22b8208de
satyamshah <34947025+satyamshah@users.noreply.github.com>
2020-05-22 23:20:49 +0530

    Create Anticlockwiseprint.cpp

---
+++ 100daysofcode/Anticlockwiseprint.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int arr[4][4] = {{1, 2, 3, 4}
+		{5, 6, 7, 8}
+		{9, 1, 2, 3}
+		{7, 8, 9, 6}
+	};
+
+	int m = 4;
+	int startrow = 0;
+	int startcolumn = 0;
+	int endrow = m - 1;
+	int endcolumn = m - 1;
+
+	for (int i = startrow; i < endrow; i++)
+	{
+		for ()
+		}
+
+}
